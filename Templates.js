98e7143b8b0de3f8f36990bdb7dfce2d71facfc8
Shwaffle <25282996+Shwaffle@users.noreply.github.com>
2018-07-28 14:45:47 -0500

    Templates
    Adding template sections to Github.

---
+++ Templates.js
@@ -0,0 +1,39 @@
+//Attack Block
+		attacks: [{
+		
+				ability: ,
+				damage: [, , ""],
+				range: "",
+
+			}, {
+
+				ability: ,
+				damage: [, , ""],
+				range: "",
+
+			},
+		],
+
+// Unarmed Attack
+
+		attacks: [{
+				name: "Unarmed Strike",
+				ability: 1,
+				damage: [1, 4, "bludgeoning"],
+				range: "Melee (5 ft)",
+				description: "",
+			},
+		],
+
+// True Empty Attack
+
+		attacks: [{
+				name: "",
+				ability: 1,
+				damage: [0, 0, ""],
+				range: "",
+				description: "",
+			},
+		],
+	
+
